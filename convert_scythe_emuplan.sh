d629a491b5117b3145183801c02b7321f91d8669
webhead404 <kvvincentvalentine@gmail.com>
2022-02-04 17:38:34 -0600

    Removed unnecessary lines

--- convert_scythe_emuplan.sh
+++ convert_scythe_emuplan.sh
@@ -12,6 +12,13 @@ OPERATOR_TTP=$(uuidgen)
 
 
 
+# Example code for converting multi line string to array. 
+# https://stackoverflow.com/questions/24628076/convert-multiline-string-to-array
+
+#IFS=$'\n'
+#for (( i=0; i<${#SPLIT_TTPS[@]}; i++ ));
+
+# Another example
 
 #while IFS= read -r line ; do 
 #(echo $line >> ${OPERATOR_TTP}.yml; done <<< "${SCYTHE_THREAT_DESCRIPTION}, ${SCYTHE_TTP}"
@@ -22,32 +29,25 @@ OPERATOR_TTP=$(uuidgen)
 #done
 
 
-#SPLIT_TTPS=($SCYTHE_TTP)
+# To test to make sure variable is correct
+#echo "${SCYTHE_TTP}"
 
-IFS=$'\n'
+# Pulls all the correct key/values from JSON file and converts over to YAML.
 
-
-
-
-
-
-#for (( i=0; i<${#SPLIT_TTPS[@]}; i++ ));
-#for (( i=0; i<${#SPLIT_TTPS[@]}; i++ ))
-#do
-echo "${SCYTHE_TTP}"
-#done
-    #echo "
-#${OPERATOR_TTP}
-#metadata:
-#  version: 1
-#  authors:
-#    - scythe-io
-#name: ${SCYTHE_THREAT_NAME}
-#description: |
-#  ${SCYTHE_THREAT_DESCRIPTION}
-#platforms:
-#  windows:
-#    cmd:
-#      command: "${SPLIT_TTPS}"
-#" > ${OPERATOR_TTP}.yml
-#done
+for LINE in "${SCYTHE_TTP}"
+do
+    echo "
+${OPERATOR_TTP}
+metadata:
+  version: 1
+  authors:
+    - scythe-io
+name: ${SCYTHE_THREAT_NAME}
+description: |
+  ${SCYTHE_THREAT_DESCRIPTION}
+platforms:
+  windows:
+    cmd:
+      command: "${SCYTHE_TTP}"
+" > ${OPERATOR_TTP}.yml
+done
